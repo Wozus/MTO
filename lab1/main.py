c4487701ab4b09d489da9f15533c273bad894549
Hubert Woziński <78148000+Wozus@users.noreply.github.com>
2023-02-27 17:33:37 +0100

    Change file format

--- lab1/main.py
+++ lab1/main.py
@@ -13,8 +13,8 @@ def change_letters(param):
 
 def my_printf(format_string,param):
     #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
+    shouldDo = True
+    for idx in range(0, len(format_string)):
         if shouldDo:
             if format_string[idx] == '#' and format_string[idx+1] == 'k':
                 print(change_letters(param), end="")
@@ -22,10 +22,10 @@ def my_printf(format_string,param):
             else:
                 print(format_string[idx], end="")
         else:
-            shouldDo=True
+            shouldDo = True
     print("")
 
-data=sys.stdin.readlines()
+data = sys.stdin.readlines()
 
-for i in range(0,len(data),2):
-    my_printf(data[i].rstrip(),data[i+1].rstrip())
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
