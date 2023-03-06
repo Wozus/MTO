70a43276ff7025119e61b13873d23b3e30e04154
Hubert <xbuenoffa@gmail.com>
2023-03-06 16:24:01 +0100

    Add lab2 files

---
+++ lab2/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import sys
+
+def my_printf(format_string, param):
+    print("")
+
+data = sys.stdin.readlines()
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i+1].rstrip())
