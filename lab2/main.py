bee930f8d2e5de21bbe33029d11602fe746a31c6
Hubert <xbuenoffa@gmail.com>
2023-03-06 16:40:12 +0100

    printf function body

--- lab2/main.py
+++ lab2/main.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
 import sys
+import re
 
 def my_printf(format_string, param):
+    match = re.search("#(\.\d*)?k", format_string)
+    if match is None:
+        match = re.search("#k", format_string)
+        replacement = format_string[match.start():match.end()]
+        print(format_string.replace(replacement, param.swapcase()), end="")
+    else:
+        replacement = format_string[match.start():match.end()]
+        max_chars = len(param)
+        if match.group(1):
+            max_chars = int(replacement[2:-1])
+
+        print(format_string.replace(replacement, param[0: max_chars].swapcase()), end="")
     print("")
 
 data = sys.stdin.readlines()
