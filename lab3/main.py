d7bd31254a13a513a918accb6525b430fd9501ab
Hubert <xbuenoffa@gmail.com>
2023-03-13 16:31:01 +0100

    change code

--- lab3/main.py
+++ lab3/main.py
@@ -4,28 +4,26 @@ import sys
 import re
 
 def my_printf(format_string, param):
-    match = re.search("#(\.\d*)?k", format_string)
-    if match is None:
-        match = re.search("#k", format_string)
-        if match is None:
-            print(format_string)
-            return
-        replacement = format_string[match.start():match.end()]
-        print(format_string.replace(replacement, param.swapcase()), end="")
+    search_group = re.search("#(\d*)?(\.\d*)?k", format_string)
+    if search_group is None:
+        print(format_string)
+        return
     else:
-        replacement = format_string[match.start():match.end()]
-        max_chars = len(param)
-        if match.group(1):
-            if replacement[2:-1].isnumeric():
-                max_chars = int(replacement[2:-1])
-            else:
-                print(format_string)
-                return
-        if max_chars < 0:
-            print(format_string, end="")
-        else:
-            print(format_string.replace(replacement, param[0: max_chars].swapcase()), end="")
-    print("")
+        swap_param = param.swapcase()
+        to_print = search_group.group(0)
+        min_length = search_group.group(1)
+        max_length = search_group.group(2)
+
+        if min_length is not None and min_length.isnumeric():
+            min_value = int(min_length)
+            if min_value > 0:
+                swap_param += ' ' * min_value
+
+        if max_length is not None and max_length.isnumeric():
+            max_value = int(max_length[1:])
+            swap_param = swap_param[:min(max_value, len(swap_param))]
+
+        print(format_string.replace(to_print, swap_param))
 
 data = sys.stdin.readlines()
 
