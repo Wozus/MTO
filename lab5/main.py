8219112c5ce3c0c48c459903f87fea170977a5d4
Hubert <xbuenoffa@gmail.com>
2023-04-03 16:26:33 +0200

    change code

--- lab5/main.py
+++ lab5/main.py
@@ -3,19 +3,22 @@
 import sys
 import re
 
-def correct_change(num):
-    if int(num) < 0:
-        tmp = num[1:]
-        return "-" + tmp[::-1]
-    return num[::-1]
+
+def change_for_xg(num):
+    tmp = [int(dec) for dec in num]
+    for j in range(len(tmp)):
+        tmp[j] = tmp[j] - 1
+        if tmp[j] == -1:
+            tmp[j] = 9
+    return int(''.join(map(str, tmp)))
 
 def my_printf(format_string, param):
-    search = re.search("#g", format_string)
-    if search is None:
+    search = re.search("#([1-9]\d*)g", format_string)
+    if search is None or param.isnumeric():
         print(format_string)
         return
     else:
-        print(format_string.replace(search.group(0), str(int(correct_change(param)))))
+        print(format_string.replace(search.group(0), str(change_for_xg(param))))
 
 data = sys.stdin.readlines()
 
