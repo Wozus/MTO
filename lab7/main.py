7fda6642b80f2e723e40773e1b4dc7ac3bf68d70
Hubert <xbuenoffa@gmail.com>
2023-04-24 16:28:25 +0200

    change code

--- lab7/main.py
+++ lab7/main.py
@@ -14,22 +14,26 @@ change = {
 
 
 def change_16(num):
-    tmp = [dec for dec in num]
+    tmp = [dec for dec in str(num)]
     for i in range(len(tmp)):
-        if change[tmp[i]]:
+        if change.get(tmp[i]):
             tmp[i] = change[tmp[i]]
+    return ''.join(map(str, tmp))
 
 def my_printf(format_string, param):
-    search = re.search("#x", format_string)
+    search = re.search("#j", format_string)
     if search is None:
         print(format_string)
         return
     else:
-        value_16 = int(param, 16)
-        change_16(value_16)
-        print(format_string.replace(search.group(0), value_16))
+        value = int(param)
+        value_16 = hex(value)
+        result = change_16(value_16)
+        print(format_string.replace(search.group(0), result))
 
-data = sys.stdin.readlines()
+# data = sys.stdin.readlines()
+#
+# for i in range(0, len(data), 2):
+#     my_printf(data[i].rstrip(), data[i + 1].rstrip())
 
-for i in range(0, len(data), 2):
-    my_printf(data[i].rstrip(), data[i + 1].rstrip())
+my_printf("#j", 12)
