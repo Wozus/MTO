99404fc2a74d0cc8ec09e3eb56c4c1ecbbae1e59
Hubert <xbuenoffa@gmail.com>
2023-05-15 17:32:52 +0200

    code

---
+++ lab8/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+change = {
+    'a': 'g',
+    'b': 'h',
+    'c': 'i',
+    'd': 'j',
+    'e': 'k',
+    'f': 'l',
+    '0': 'o'
+}
+
+def change_16(num):
+    tmp = [dec for dec in str(num)]
+    for iter in range(len(tmp)):
+        if change.get(tmp[iter]):
+            tmp[iter] = change[tmp[iter]]
+    return ''.join(map(str, tmp))
+
+def my_printf(format_string, param):
+    search = re.search("#.([1-9]\d*)j", format_string)
+    if search is None:
+        print(format_string)
+        return
+    else:
+        value = int(param)
+        value_16 = hex(value)
+        value_16_str = str(value_16)[2:]
+        print_max = int(search.group(1))
+        param_len = len(param)
+
+        size = max(0, print_max - param_len)
+        value_16_str = '0' * size + value_16_str
+
+        result = change_16(value_16_str)
+        print(format_string.replace(search.group(0), result))
+
+data = sys.stdin.readlines()
+
+for i in range(0, len(data), 2):
+    my_printf(data[i].rstrip(), data[i + 1].rstrip())
