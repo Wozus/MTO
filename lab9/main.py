184794d281378593bfa8dd2575cb68975d9e7220
Hubert <xbuenoffa@gmail.com>
2023-05-22 17:49:48 +0200

    code

--- lab9/main.py
+++ lab9/main.py
@@ -4,43 +4,55 @@ import sys
 import re
 
 change = {
-    'a': 'g',
-    'b': 'h',
-    'c': 'i',
-    'd': 'j',
-    'e': 'k',
-    'f': 'l',
-    '0': 'o'
+    '0': 'a',
+    '1': 'b',
+    '2': 'c',
+    '3': 'd',
+    '4': 'e',
+    '5': 'f',
+    '6': 'g',
+    '7': 'h',
+    '8': 'i',
+    '9': 'j'
 }
 
-def change_16(num):
+def change_before(num):
+    num = str(num).split('.')[0]
     tmp = [dec for dec in str(num)]
     for iter in range(len(tmp)):
         if change.get(tmp[iter]):
             tmp[iter] = change[tmp[iter]]
     return ''.join(map(str, tmp))
 
+def change_after(num):
+    num = str(num).split('.')[1]
+    tmp = [dec for dec in str(num)]
+    for iter in range(len(tmp)):
+        tmp[iter] = str((int(tmp[iter]) + 5) % 10)
+    return ''.join(map(str, tmp))
+
+def correct_length(value, length):
+    splited = str(value).split('.')
+    length_c = max(0, length - len(splited[1]))
+    return str(value) + ('0' * length_c)
+
 def my_printf(format_string, param):
-    search = re.search("#.([1-9]\d*)j", format_string)
+    search = re.search("#.([1-9]\d*)h", format_string)
     if search is None:
         print(format_string)
         return
-    else:
-        if param.isnumeric() and search.group(1).isnumeric() and int(search.group(1)) >= 0:
-            value = int(param)
-            value_16 = hex(value)
-            value_16_str = str(value_16)[2:]
-            print_max = int(search.group(1))
-            param_len = len(param)
 
-            size = max(0, print_max - param_len)
-            value_16_str = '0' * size + value_16_str
+    param_value = float(param)
+    format_value = int(search.group(1))
+    round_value = round(param_value, ndigits=format_value)
 
-            result = change_16(value_16_str)
-            print(format_string.replace(search.group(0), result))
-        else:
-            print(format_string)
-            return
+    round_value = correct_length(round_value, format_value)
+    after_first = change_before(round_value)
+    after_second = change_after(round_value)
+
+    print(format_string.replace(search.group(0), after_first + '.' + after_second))
+
+
 
 data = sys.stdin.readlines()
 
