1dcd28cfed717d99bb35f25bb7d030f05a5fb359
Hubert <xbuenoffa@gmail.com>
2023-05-29 18:18:21 +0200

    all tests works

--- lab10/main.py
+++ lab10/main.py
@@ -2,13 +2,12 @@
 
 import sys
 import re
-import math
 
 def count_digits(n):
     if n == 0:
         digits = 1
     else:
-        digits = int(math.log10(abs(n))) + 1
+        digits = len(str(abs(n)))
     return digits
 
 
