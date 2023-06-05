caf2d1058a80b2bfd42985d5952fe7d7c21198cf
Hubert <xbuenoffa@gmail.com>
2023-06-05 17:36:03 +0200

    in progress 2

--- lab11/main.py
+++ lab11/main.py
@@ -7,21 +7,28 @@ data = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
 
 
 def change_num(number):
-    if n == 0:
-        digits = 1
-    else:
-        digits = len(str(abs(n)))
-    return digits
-
-
+    nums = [dec for dec in str(number)]
+    counter = 0
+    for i in range(len(nums) - 1, -1, -1):
+        if nums[i] == '1':
+            nums[i] = data[counter % 10]
+        else:
+            nums[i] = '0'
+        counter += 1
+    return ''.join(map(str, nums))
 def my_printf(format_string, param):
     search = re.search("#b", format_string)
     if search is None:
         print(format_string)
         return
+    num_bin = bin(int(param))
+    num_bin = num_bin.lstrip('0')
+    res = change_num(num_bin)
+    print(res)
 
+# data = sys.stdin.readlines()
+#
+# for i in range(0, len(data), 2):
+#    my_printf(data[i].rstrip(), data[i + 1].rstrip())
 
-data = sys.stdin.readlines()
-
-for i in range(0, len(data), 2):
-   my_printf(data[i].rstrip(), data[i + 1].rstrip())
+my_printf("#b", "123123123")
